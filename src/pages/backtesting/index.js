3f2a1ba292df9b0713f7ebf7bc1fef68841766a2
Léo LEGRON <hoolasataf@gmail.com>
2022-06-30 11:00:51 +0200

    wip

--- src/pages/backtesting/index.js
+++ src/pages/backtesting/index.js
@@ -7,6 +7,7 @@ import "./index.css";
 const BacktestingPage = (props) => {
 
     const [data, setData] = useState(null);
+    // eslint-disable-next-line
     const [error, setError] = useState(null);
 
     const [startDate, setStartDate] = useState("01-01-2022");
@@ -14,7 +15,9 @@ const BacktestingPage = (props) => {
     const [symbol, setSymbol] = useState("ETH");
     const [type, setType] = useState("sma");
     const [unit, setUnit] = useState("day");
-    const [periods, setPeriods] = useState([25, 100]);
+    const [period1, setPeriod1] = useState(7);
+    const [period2, setPeriod2] = useState(25);
+    const [period3, setPeriod3] = useState(99);
 
     const applyChanges = useCallback(() => {
         if(
@@ -22,14 +25,13 @@ const BacktestingPage = (props) => {
             endDate !== "" &&
             symbol !== "" &&
             type !== "" &&
-            unit !== "" &&
-            periods.length > 0
+            unit !== ""
         ){
-            apiService.backtest(startDate, endDate, symbol, type, unit, periods)
+            apiService.backtest(startDate, endDate, symbol, type, unit, [period1, period2, period3])
                 .then(setData)
                 .catch(setError);
         }
-    }, [startDate, endDate, symbol, type, unit, periods])
+    }, [startDate, endDate, symbol, type, unit, period1, period2, period3])
 
     return (
         <div className={"backtesting-container"}>
@@ -39,14 +41,10 @@ const BacktestingPage = (props) => {
                 <input type={"text"} value={symbol} onChange={(ev) => setSymbol(ev.target.value)}/>
                 <input type={"text"} value={type} onChange={(ev) => setType(ev.target.value)}/>
                 <input type={"text"} value={unit} onChange={(ev) => setUnit(ev.target.value)}/>
-                {
-                    periods.map((period, i) => (
-                        <input key={i} type={"text"} value={period} onChange={(ev) => {
-                            periods[i] = period;
-                            setPeriods(periods);
-                        }}/>
-                    ))
-                }
+                <input type={"text"} value={period1} onChange={(ev) => setPeriod1(ev.target.value)}/>
+                <input type={"text"} value={period2} onChange={(ev) => setPeriod2(ev.target.value)}/>
+                <input type={"text"} value={period3} onChange={(ev) => setPeriod3(ev.target.value)}/>
+
                 <div onClick={applyChanges}>backtest</div>
             </div>
             <div className={"backtesting-graph"}>
